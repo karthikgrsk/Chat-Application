0173071d27e1709c0ce4dac14ffe6f98e2b002c7
Karthik G <156653445+karthikgrsk@users.noreply.github.com>
2025-08-06 12:46:03 +0000

    added chat.html

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints (StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("https://localhost:8080")
+                .setAllowedOrigins("http://localhost:8080")
                 .withSockJS();
     }
 
