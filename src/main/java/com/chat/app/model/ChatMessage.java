31bd2020d367c9d58098a94474c4afd73cf35901
Karthik G <156653445+karthikgrsk@users.noreply.github.com>
2025-08-07 16:34:56 +0000

    update chat.html

--- src/main/java/com/chat/app/model/ChatMessage.java
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChatMessage {
     private int id;
-    private int sender;
+    private String sender;
     private String content;
 }
